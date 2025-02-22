a04daf9b66c85d71611086e744492a89f062e22a
Manoel Brunnen <manoel.brunnen@orgatex.de>
2025-02-22 23:33:43 +0100

    Add F3 to spi clock calc

--- src/main/drivers/bus_spi.c
+++ src/main/drivers/bus_spi.c
@@ -241,8 +241,8 @@ void spiBusSetInstance(busDevice_t *bus, SPI_TypeDef *instance)
 }
 
 uint16_t spiCalculateDivider(uint32_t freq)
+#if defined(STM32F3) || defined(STM32F4) || defined(STM32G4) || defined(STM32F7)
 {
-#if defined(STM32F4) || defined(STM32G4) || defined(STM32F7)
     uint32_t spiClk = SystemCoreClock / 2;
 #elif defined(STM32H7)
     uint32_t spiClk = 100000000;
